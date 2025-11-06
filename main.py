4209b1919437510e696d7151bf387e280971b0a6
bigtimer-dev <aljnoma@gmail.com>
2025-11-06 00:48:55 -0400

    So we got an ai agent now working, careful with it: no security take in mind

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ client = genai.Client(
     api_key=api_key
 )  # assing the client with the api key for response
 
-
+# declare functions for the llm use
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
@@ -31,16 +31,19 @@ available_functions = types.Tool(
 )
 
 
+# enum for match cases
 class options(Enum):
     no_verbose = 2
     with_verbose = 3
 
 
+# getting message_roles
 def message(usr_prompt):
     messages = [types.Content(role="user", parts=[types.Part(text=usr_prompt)])]
     return messages
 
 
+# the generate response from the ai with the configs to the llm and the available_functions to use
 def response_from_ia(messages):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
@@ -55,45 +58,88 @@ def response_from_ia(messages):
 def main():
     usr_prompt = sys.argv[1]
     message_roles = message(usr_prompt)
-    response = response_from_ia(message_roles)
 
-    # using the command line to ask for a user prompt
-    match len(sys.argv):
-        case options.no_verbose.value:
-            if response.function_calls:
-                for item in response.function_calls:
-                    result = call_function(item, False)
-                    parts = result.parts
-                    if not parts or not getattr(parts[0], "function_response", None):
-                        raise RuntimeError("Missing function_response in tool content")
+    try:
+        for i in range(20):
+            response = response_from_ia(message_roles)
+            for candidate in response.candidates:
+                message_roles.append(candidate.content)
 
-            else:
-                print(response.text)
+            # using the command line to ask for a user prompt
+            match len(sys.argv):
+                case options.no_verbose.value:
+                    if response.function_calls:
+                        for item in response.function_calls:
+                            result = call_function(item, False)
+                            parts = result.parts
 
-        case options.with_verbose.value if sys.argv[2] == "--verbose":
-            if response.function_calls:
-                for item in response.function_calls:
-                    result = call_function(item, True)
-                    parts = result.parts
-                    if not parts or not getattr(parts[0], "function_response", None):
-                        raise RuntimeError("Missing function_response in tool content")
-                    print(f"-> {result.parts[0].function_response.response}")
+                            if not parts or not getattr(
+                                parts[0], "function_response", None
+                            ):
+                                raise RuntimeError(
+                                    "Missing function_response in tool content"
+                                )
 
-            else:
-                print(f"User prompt:\n {usr_prompt}\n")
-                print(f"gemini-2 response:\n {response.text}")
-                print(
-                    f"Prompt tokens: {response.usage_metadata.prompt_token_count}"
-                )  # prompt_token_count
-                print(
-                    f"Response tokens: {response.usage_metadata.candidates_token_count}"
-                )  # response_token_count
+                            message_roles.append(
+                                types.Content(
+                                    role="user",
+                                    parts=[
+                                        types.Part(
+                                            text=f"{parts[0].function_response.response}"
+                                        )
+                                    ],
+                                )
+                            )
+                        continue
 
-        case _:
-            print(
-                "Usage of the tool: uv run main.py <'usr_prompt'> [optional: --verbose]"
-            )
-            sys.exit(1)
+                    else:
+                        print(response.text)
+                        break
+
+                case options.with_verbose.value if sys.argv[2] == "--verbose":
+                    if response.function_calls:
+                        for item in response.function_calls:
+                            result = call_function(item, True)
+                            parts = result.parts
+                            if not parts or not getattr(
+                                parts[0], "function_response", None
+                            ):
+                                raise RuntimeError(
+                                    "Missing function_response in tool content"
+                                )
+
+                            message_roles.append(
+                                types.Content(
+                                    role="user",
+                                    parts=[
+                                        types.Part(
+                                            text=f"{parts[0].function_response.response}"
+                                        )
+                                    ],
+                                )
+                            )
+
+                            print(f"-> {result.parts[0].function_response.response}")
+                        continue
+
+                    else:
+                        print(f"User prompt:\n {usr_prompt}\n")
+                        print(f"gemini-2 response:\n {response.text}")
+                        print(
+                            f"Prompt tokens: {response.usage_metadata.prompt_token_count}"
+                        )  # prompt_token_count
+                        print(
+                            f"Response tokens: {response.usage_metadata.candidates_token_count}"
+                        )  # response_token_count
+                        break
+
+                case _:
+                    print(
+                        "Usage of the tool: uv run main.py <'usr_prompt'> [optional: --verbose]"
+                    )
+                    sys.exit(1)
+    except Exception as e:
+        return f"Error:{e}"
 
 
 if __name__ == "__main__":
--- system_prompt_config.py
+++ system_prompt_config.py
@@ -1,7 +1,7 @@
 system_prompt = """
 You are a helpful AI coding agent.
 
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+When a user asks a question or makes a request try to solve what he is asking base to your allow directory, see if there are any correlated files or directory that you need to explorer to try to answer its question, make a function call plan.If you gonna overwrite a file with write_file first read its contents and the new contents will be the before one with the fix you did to it. You can perform the following operations:
 
 - List files and directories
 - Read file contents
